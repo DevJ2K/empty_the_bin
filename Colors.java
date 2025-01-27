2bb62783a15585396fbc1ab89cedb2bc9c194fc3
J2K <116686309+DevJ2K@users.noreply.github.com>
2025-01-27 10:57:40 +0100

    ✅ : Using enum instead of class...

--- Colors.java
+++ Colors.java
@@ -1,76 +1,85 @@
-public final class Colors {
-    private Colors() {}
-
+public enum Colors {
     // Regular text
-    public static final String BLACK = "\033[0;30m";
-    public static final String RED = "\033[0;31m";
-    public static final String GREEN = "\033[0;32m";
-    public static final String YELLOW = "\033[0;33m";
-    public static final String BLUE = "\033[0;34m";
-    public static final String MAG = "\033[0;35m";
-    public static final String CYAN = "\033[0;36m";
-    public static final String WHITE = "\033[0;37m";
+    BLACK("\033[0;30m"),
+    RED("\033[0;31m"),
+    GREEN("\033[0;32m"),
+    YELLOW("\033[0;33m"),
+    BLUE("\033[0;34m"),
+    MAG("\033[0;35m"),
+    CYAN("\033[0;36m"),
+    WHITE("\033[0;37m"),
 
     // Regular bold text
-    public static final String BBLACK = "\033[1;30m";
-    public static final String BRED = "\033[1;31m";
-    public static final String BGREEN = "\033[1;32m";
-    public static final String BYELLOW = "\033[1;33m";
-    public static final String BBLUE = "\033[1;34m";
-    public static final String BMAG = "\033[1;35m";
-    public static final String BCYAN = "\033[1;36m";
-    public static final String BWHITE = "\033[1;37m";
+    BBLACK("\033[1;30m"),
+    BRED("\033[1;31m"),
+    BGREEN("\033[1;32m"),
+    BYELLOW("\033[1;33m"),
+    BBLUE("\033[1;34m"),
+    BMAG("\033[1;35m"),
+    BCYAN("\033[1;36m"),
+    BWHITE("\033[1;37m"),
 
     // Regular underline text
-    public static final String UBLACK = "\033[4;30m";
-    public static final String URED = "\033[4;31m";
-    public static final String UGREEN = "\033[4;32m";
-    public static final String UYELLOW = "\033[4;33m";
-    public static final String UBLUE = "\033[4;34m";
-    public static final String UMAG = "\033[4;35m";
-    public static final String UCYAN = "\033[4;36m";
-    public static final String UWHITE = "\033[4;37m";
+    UBLACK("\033[4;30m"),
+    URED("\033[4;31m"),
+    UGREEN("\033[4;32m"),
+    UYELLOW("\033[4;33m"),
+    UBLUE("\033[4;34m"),
+    UMAG("\033[4;35m"),
+    UCYAN("\033[4;36m"),
+    UWHITE("\033[4;37m"),
 
     // Regular background
-    public static final String BLACKB = "\033[40m";
-    public static final String REDB = "\033[41m";
-    public static final String GREENB = "\033[42m";
-    public static final String YELLOWB = "\033[43m";
-    public static final String BLUEB = "\033[44m";
-    public static final String MAGB = "\033[45m";
-    public static final String CYANB = "\033[46m";
-    public static final String WHITEB = "\033[47m";
+    BLACKB("\033[40m"),
+    REDB("\033[41m"),
+    GREENB("\033[42m"),
+    YELLOWB("\033[43m"),
+    BLUEB("\033[44m"),
+    MAGB("\033[45m"),
+    CYANB("\033[46m"),
+    WHITEB("\033[47m"),
 
     // High intensity background
-    public static final String BLACKHB = "\033[0;100m";
-    public static final String REDHB = "\033[0;101m";
-    public static final String GREENHB = "\033[0;102m";
-    public static final String YELLOWHB = "\033[0;103m";
-    public static final String BLUEHB = "\033[0;104m";
-    public static final String MAGHB = "\033[0;105m";
-    public static final String CYANHB = "\033[0;106m";
-    public static final String WHITEHB = "\033[0;107m";
+    BLACKHB("\033[0;100m"),
+    REDHB("\033[0;101m"),
+    GREENHB("\033[0;102m"),
+    YELLOWHB("\033[0;103m"),
+    BLUEHB("\033[0;104m"),
+    MAGHB("\033[0;105m"),
+    CYANHB("\033[0;106m"),
+    WHITEHB("\033[0;107m"),
 
     // High intensity text
-    public static final String HBLACK = "\033[0;90m";
-    public static final String HRED = "\033[0;91m";
-    public static final String HGREEN = "\033[0;92m";
-    public static final String HYELLOW = "\033[0;93m";
-    public static final String HBLUE = "\033[0;94m";
-    public static final String HMAG = "\033[0;95m";
-    public static final String HCYAN = "\033[0;96m";
-    public static final String HWHITE = "\033[0;97m";
+    HBLACK("\033[0;90m"),
+    HRED("\033[0;91m"),
+    HGREEN("\033[0;92m"),
+    HYELLOW("\033[0;93m"),
+    HBLUE("\033[0;94m"),
+    HMAG("\033[0;95m"),
+    HCYAN("\033[0;96m"),
+    HWHITE("\033[0;97m"),
 
     // Bold high intensity text
-    public static final String BHBLACK = "\033[1;90m";
-    public static final String BHRED = "\033[1;91m";
-    public static final String BHGREEN = "\033[1;92m";
-    public static final String BHYELLOW = "\033[1;93m";
-    public static final String BHBLUE = "\033[1;94m";
-    public static final String BHMAG = "\033[1;95m";
-    public static final String BHCYAN = "\033[1;96m";
-    public static final String BHWHITE = "\033[1;97m";
+    BHBLACK("\033[1;90m"),
+    BHRED("\033[1;91m"),
+    BHGREEN("\033[1;92m"),
+    BHYELLOW("\033[1;93m"),
+    BHBLUE("\033[1;94m"),
+    BHMAG("\033[1;95m"),
+    BHCYAN("\033[1;96m"),
+    BHWHITE("\033[1;97m"),
 
     // Reset
-    public static final String RESET = "\033[0m";
+    RESET("\033[0m");
+
+    private final String color;
+
+    private Colors(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return this.color;
+    }
 }
